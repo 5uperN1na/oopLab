41f7f89acbb61254a6ac39a91ea68b63daa21cd6
Paula Suarez <psuarez@dickeys2.local>
2018-11-13 08:30:28 -0600

    Adding updates to files

--- app1.js
+++ app1.js
@@ -1,17 +1,25 @@
 //Random Square Game using classes and methods
 
+const MAX = 800;
+let container = document.getElementById('square-container');
+
 class Square {
+    //constructor
     constructor(x, y) {
-        this.div = createElement('div');
+        this.div = document.createElement('div');
         this.div.classList.add('square');
         this.div.style.left = `${x}px`;
         this.div.style.top = `${y}px`;
+        this.updateColor();
+        container.appendChild(this.div);
 
     }
 
+    //method
     updateColor() {
-        //let randomColor = `rb (124, 117, 255)`;
+        //let randomColor = `rb (117, 24, 32)`;
         let randomColor = `rgb(${randomVal(0, 255)}, ${randomVal(0, 255)}, ${randomVal(0, 255)})`;
+        this.div.style.backgroundColor = randomColor;
     }
 
 }
@@ -19,7 +27,9 @@ let sqButton = document.getElementById('square-button');
 sqButton.addEventListener('click', insertSquare);
 
 function insertSquare() {
-    alert("works!");
+    let xVal = randomVal(0, MAX);
+    let yVal = randomVal(0, MAX);
+    let sq = new Square(xVal, yVal);
 
 }
 
