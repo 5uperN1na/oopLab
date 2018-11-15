fa665bf0ca607e239fac66de585edc7eaebf2a54
Paula Suarez <psuarez@dickeys2.local>
2018-11-15 08:16:57 -0600

    Adding updates to files.

--- app1.js
+++ app1.js
@@ -5,34 +5,58 @@ let container = document.getElementById('square-container');
 
 class Square {
     //constructor
-    constructor(x, y) {
+    constructor(x, y, size) {
         this.div = document.createElement('div');
         this.div.classList.add('square');
         this.div.style.left = `${x}px`;
         this.div.style.top = `${y}px`;
+        this.div.style.width = `${size}px`;
+        this.div.style.height = `${size}px`;
+        //binding the square to the div so update function can work
+        //this.div.addEventListener('click', function () {
+        //this.updateColor();
+        // }.bind(this));
+
+        //ES2016 arrow function and function context (constructor creating a div/square and arrow function refers to square)
+        this.div.addEventListener('click', () => {
+            console.log(this);
+            this.updateColor();
+            this.updateLocation();
+
+        });
+
         this.updateColor();
         container.appendChild(this.div);
-        console.log ('true');
 
     }
 
-    //method
+    //method to randoming change color of square
     updateColor() {
         //let randomColor = `rb (117, 24, 32)`;
         let randomColor = `rgb(${randomVal(0, 255)}, ${randomVal(0, 255)}, ${randomVal(0, 255)})`;
         this.div.style.backgroundColor = randomColor;
     }
 
+    //method to randoming change X and Y coordinates 
+    updateLocation() {
+        let xVal = randomVal(0, MAX);
+        let yVal = randomVal(0, MAX);
+        this.div.style.left = `${xVal}px`;
+        this.div.style.top = `${yVal}px`;
+    }
+
+
 }
 let sqButton = document.getElementById('square-button');
 sqButton.addEventListener('click', insertSquare);
-console.log('true again');
+
 
 function insertSquare() {
     let xVal = randomVal(0, MAX);
     let yVal = randomVal(0, MAX);
-    let sq = new Square(xVal, yVal);
-
+    let size = randomVal(25, 100);
+    let sq = new Square(xVal, yVal, size);
+   
 }
 
 function randomVal(min, max) {
